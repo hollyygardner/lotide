7974e7fc771c6cc9a36272ab103525fbb3957b45
Holly Gardner <hollylizgardner@gmail.com>
2022-05-02 19:49:46 -0400

    Creating a new assertion function with emojis

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: 1 === 1");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: Lighthouse Labs !== Bootcamp");
+  }
+};

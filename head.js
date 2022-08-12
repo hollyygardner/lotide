65b35f6891b564ed2002800b891daaa6a8459080
Holly Gardner <hollylizgardner@gmail.com>
2022-08-12 19:44:14 -0400

    Refactored

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual}) !== ${expected}`);
-  }
-}; 
-
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0]
 }
-
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head

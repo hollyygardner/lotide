716d1a7fd41aae1feb9f5284afcbae8b2367ccc9
Holly Gardner <hollylizgardner@gmail.com>
2022-08-14 19:00:03 -0400

    Refactored

---
+++ assertArraysEqual2.js
@@ -0,0 +1,15 @@
+
+const eqArrays = require('./eqArrays')
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let answer = false
+  if (Array.isArray(arrayOne) && Array.isArray(arrayTwo)) {
+    answer = eqArrays(arrayOne, arrayTwo)
+  } if (answer === true) {
+    console.log(`😃😃😃Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+}
+
+module.exports = assertArraysEqual

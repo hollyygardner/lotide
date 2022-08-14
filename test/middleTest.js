1598eb7351958dba936ff4634bb6e00d405dfdac
Holly Gardner <hollylizgardner@gmail.com>
2022-08-14 19:27:11 -0400

    test with Mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
-const middle = require('../middle')
+const assert = require("chai").assert;
+const middle = require("../middle")
 
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+describe("#middle", () => {
+  it("should return the two middle elements of even array", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+  it("should return the middle element of odd array", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it("should return an empty array when given an array length of 2", () => {
+    assert.deepEqual(middle([5,'6']), []);
+    });
+  it("should return an empty array when given an array length of 1", () => {
+    assert.deepEqual(middle([1]), []);
+    });
+  it("should return an empty array when given empty array", () => {
+    assert.deepEqual(middle([]), []);
+    });
+});

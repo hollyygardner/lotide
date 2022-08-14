89f74f2e2332b6e95522793fb1aa967561154515
Holly Gardner <hollylizgardner@gmail.com>
2022-08-14 19:11:59 -0400

    Refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: ${actual} === ${expected}");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: ${actual}) !== {expected}");
-  }
-}; 
-
-const eqArrays = function(arrayOne, arrayTwo) {
-  let answer = true;
-  for (let i = 0; i < arrayOne.length; i ++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      answer = false;
-    }
-  }
-
-
-  return answer;
-  }
-
  const middle = function(array) {
    let output = true; 
      if (array.length <= 2) {
@@ -29,9 +9,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
  }
 }
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle
